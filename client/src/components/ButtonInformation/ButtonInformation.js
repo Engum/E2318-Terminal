e3849023314323d3dfd53a799bcdde81cabec890
idarev <idarev@stud.ntnu.no>
2023-05-07 23:30:05 +0200

    Latest update V2

---
+++ client/src/components/ButtonInformation/ButtonInformation.js
@@ -0,0 +1,12 @@
+import "./ButtonInformation.css";
+import arrow from "../../assets/arrow.svg";
+
+export const ButtonInformation = () => {
+  return (
+    <div className="flex-column bi-wrapper">
+      <h1>PRESS THE BUTTON BELOW</h1>
+      <h2>To order the ferry</h2>
+      <img src={arrow} alt="arrow icon" />
+    </div>
+  );
+};
